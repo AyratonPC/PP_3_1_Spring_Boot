71bee0646f74bf4cf98161c04bc00eac4d5af148
Айрат Латыпов <ayrat.latypov.2020@bk.ru>
2024-05-11 03:35:32 +0300

    fix

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,11 +1,7 @@
 package com.example.demo.repository;
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-@Repository
-@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,22 +16,30 @@ public class UserServiceImpl implements UserService {
         this.userRepository = userRepository;
     }
 
+    @Override
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
+    @Override
+    @Transactional
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
+    @Override
+    @Transactional
     public void update(User user) {
         userRepository.save(user);
     }
 
+    @Override
+    @Transactional
     public void delete(long id) {
         userRepository.delete(show(id));
     }
 
+    @Override
     public User show(long id) {
         return userRepository.getReferenceById(id);
     }
